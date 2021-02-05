e8f847f9f9448f24ccf59e8a9fd4a3cb09c17856
Daniel Williams <dancart2001@gmail.com>
2021-02-05 12:41:21 +0000

    fixed collision system bug

--- src/main/java/teamproject/wipeout/App.java
+++ src/main/java/teamproject/wipeout/App.java
@@ -28,17 +28,23 @@ public class App extends Application {
         GameLoop gl = new GameLoop(systemUpdater, renderer);
         
         GameEntity nge = gameScene.createEntity();
-        nge.addComponent(new Transform(250,250));
-        nge.addComponent(new RectRenderComponent(Color.DARKRED, 20, 20));
-        nge.addComponent(new MovementComponent(80f, -100f, -20f, 40f));
+        nge.addComponent(new Transform(200,400));
+        nge.addComponent(new RectRenderComponent(Color.MEDIUMPURPLE, 20, 20));
+        nge.addComponent(new MovementComponent(80f, 0, -20f, 60f));
         nge.addComponent(new CollisionComponent());
 
         //add more entities here
         GameEntity platform = gameScene.createEntity();
         platform.addComponent(new Transform(300,300));
-        platform.addComponent(new RectRenderComponent(Color.DARKRED, 200, 200));
+        platform.addComponent(new RectRenderComponent(Color.DARKGRAY, 200, 200));
         platform.addComponent(new MovementComponent(0, 0, 0, 0));
         platform.addComponent(new CollisionComponent());
+        
+        GameEntity nge2 = gameScene.createEntity();
+        nge2.addComponent(new Transform(250,300));
+        nge2.addComponent(new RectRenderComponent(Color.DARKRED, 20, 20));
+        nge2.addComponent(new MovementComponent(50f, -100f, 0, 80f));
+        nge2.addComponent(new CollisionComponent());
         
         Scene scene = new Scene(new StackPane(canvas), 800, 600);
         stage.setScene(scene);
--- src/main/java/teamproject/wipeout/engine/system/CollisionSystem.java
+++ src/main/java/teamproject/wipeout/engine/system/CollisionSystem.java
@@ -25,24 +25,23 @@ public class CollisionSystem implements GameSystem {
 
         for(int i=0; i < entities.size(); i++) {
             for(int j=0; j < entities.size(); j++) {
-            	if(i==j) {
-            		break;
-            	}
-            	if(collides(entities.get(i), entities.get(j))) {
-            		
-            		//TODO make sure components behave correctly now we have detected the collision
-            		//This is a quick hacky thing just to see if it works
-            		//Only works because both components have a gravity and velocity component 
-            		//at the moment a collision causes both objects to freeze
-            		MovementComponent m1 = entities.get(i).getComponent(MovementComponent.class);
-                    m1.velocity= new Point2D(0,0);
-                    m1.acceleration = new Point2D(0,0);
-                    
-                    MovementComponent m2 = entities.get(j).getComponent(MovementComponent.class);
-                    m2.velocity= new Point2D(0,0);
-                    m2.acceleration = new Point2D(0,0);
-                    
-                    
+            	if(i!=j) {
+                	if(collides(entities.get(i), entities.get(j))) {
+                		
+                		//TODO make sure components behave correctly now we have detected the collision
+                		//This is a quick hacky thing just to see if it works
+                		//Only works because both components have a gravity and velocity component 
+                		//at the moment a collision causes both objects to freeze
+                		MovementComponent m1 = entities.get(i).getComponent(MovementComponent.class);
+                        m1.velocity= new Point2D(0,0);
+                        m1.acceleration = new Point2D(0,0);
+                        
+                        MovementComponent m2 = entities.get(j).getComponent(MovementComponent.class);
+                        m2.velocity= new Point2D(0,0);
+                        m2.acceleration = new Point2D(0,0);
+                        
+                        
+                	}
             	}
             }
         }
