c908ccafe71546ae09605982703dea184b7d1b1e
Marian Vinicay <mv@marvin.tech>
2021-02-08 13:42:43 +0100

    Removed underscore from variable names

--- src/main/java/teamproject/wipeout/engine/component/audio/AudioComponent.java
+++ src/main/java/teamproject/wipeout/engine/component/audio/AudioComponent.java
@@ -7,20 +7,20 @@ import teamproject.wipeout.engine.component.GameComponent;
 
 public final class AudioComponent implements GameComponent {
 
-	private Boolean _play; //set to True when the sound needs to be played
+	private Boolean play; //set to True when the sound needs to be played
 	private final Media media;
 	private double volume;
 	
 	/**
 	 * This is a component class for adding sound effects to entities.
-	 * The AudioSystem checks the boolean attribute {@link #_play} to decide if the sound needs to play.
+	 * The AudioSystem checks the boolean attribute {@link #play} to decide if the sound needs to play.
 	 * @param audioFileName	name of the audio file
 	 */
 	public AudioComponent(String audioFileName) {
 		String filePath = this.getClass().getClassLoader().getResource("audio/" + audioFileName).toString(); // TODO: Handle null value -> throw exception
 
 		this.media = new Media(filePath);
-		this._play = false;
+		this.play = false;
 		this.volume = 1.0; //initially set to full volume
 	}
 	   
@@ -36,18 +36,18 @@ public final class AudioComponent implements GameComponent {
 		MediaPlayer player = new MediaPlayer(media); // TODO: Maybe have only one MediaPlayer per AudioComponent -> if we need better performance later
 		player.setVolume(volume);
 		player.play();
-		_play = false; //switched back to false so the sound only plays once.
+		play = false; //switched back to false so the sound only plays once.
 	}
 	
 	/**
 	 * called by any class when it wants the sound to play.
 	 */
 	public void play() {
-		_play = true; //sets the Boolean attribute to true which SoundSystem will pick up on.
+		play = true; //sets the Boolean attribute to true which SoundSystem will pick up on.
 	}
 	
 	public boolean toPlay() {
-		return _play; //used to see if the sound needs to be played.
+		return play; //used to see if the sound needs to be played.
 	}
 	
 	/**
