b788e004315c5e8916ef5c243ff86a400253ac20
Daniel Williams <dancart2001@gmail.com>
2021-02-26 13:25:43 +0000

    merging new collisions

--- src/main/java/teamproject/wipeout/engine/system/CollisionSystem.java
+++ src/main/java/teamproject/wipeout/engine/system/CollisionSystem.java
@@ -5,13 +5,6 @@ import java.util.List;
 import java.util.Set;
 
 import javafx.geometry.Point2D;
-<<<<<<< HEAD
-import javafx.scene.shape.Shape;
-import teamproject.wipeout.engine.component.Transform;
-import teamproject.wipeout.engine.component.physics.MovementComponent;
-import teamproject.wipeout.engine.component.physics.Pair;
-import teamproject.wipeout.engine.component.physics.CollisionComponent;
-=======
 import teamproject.wipeout.engine.component.Transform;
 import teamproject.wipeout.engine.component.physics.MovementComponent;
 import teamproject.wipeout.engine.component.physics.Pair;
@@ -20,7 +13,6 @@ import teamproject.wipeout.engine.component.physics.CollisionResolutionComponent
 import teamproject.wipeout.engine.component.physics.FacingDirection;
 import teamproject.wipeout.engine.component.physics.GeometryUtil;
 import teamproject.wipeout.engine.component.physics.HitboxComponent;
->>>>>>> collisions
 import teamproject.wipeout.engine.core.GameScene;
 import teamproject.wipeout.engine.entity.GameEntity;
 import teamproject.wipeout.engine.entity.collector.SignatureEntityCollector;
@@ -60,38 +52,6 @@ public class CollisionSystem implements GameSystem {
         }
         
     }
-    
-<<<<<<< HEAD
-    public void resolveCollision(GameEntity g1, GameEntity g2, Pair<Shape, Shape> p) {
-    	Point2D resolutionVector = CollisionComponent.getResolutionVector(p.first,p.second);
-    	if (resolutionVector==null) {
-    		return;
-    	}
-    	
-    	Transform t1 = g1.getComponent(Transform.class);
-    	CollisionComponent c1 = g1.getComponent(CollisionComponent.class);
-    	MovementComponent m1 = g1.getComponent(MovementComponent.class);
-    	
-    	Transform t2 = g2.getComponent(Transform.class);
-    	CollisionComponent c2 = g2.getComponent(CollisionComponent.class);
-    	MovementComponent m2 = g2.getComponent(MovementComponent.class);
-    	    	
-    	
-    	if(c1.isMoveable) {
-        	if(c2.isMoveable) {
-        		t1.setPosition(t1.getPosition().add(resolutionVector.multiply(0.5)));
-        		t2.setPosition(t2.getPosition().add(resolutionVector.multiply(-0.5)));
-        	}
-        	else {
-        		t1.setPosition(t1.getPosition().add(resolutionVector));
-        	}
-    	}
-    	else if(c2.isMoveable) {
-    		t2.setPosition(t2.getPosition().add(resolutionVector.multiply(-1)));
-    	}
-    }
-=======
->>>>>>> collisions
     
 
 
