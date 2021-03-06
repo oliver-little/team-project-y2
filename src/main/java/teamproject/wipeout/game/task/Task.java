4a3c34f95af5155cd0c0cebbf024fd01e30fd24f
Valeria <vxp999@student.bham.ac.uk>
2021-03-06 23:28:10 +0200

    feat: add task id

--- src/main/java/teamproject/wipeout/game/task/Task.java
+++ src/main/java/teamproject/wipeout/game/task/Task.java
@@ -8,6 +8,7 @@ import java.util.function.Function;
 /** Defines a task that a player has to complete to get a certain amount of money **/
 
 public class Task {
+    public Integer id;
     public Integer reward;
     public String description;
     public Boolean completed;
@@ -20,7 +21,8 @@ public class Task {
      * @param reward - how much money does the player get when they complete the task
      * @param condition - the condition under which the task is considered completed by the player
      */
-    public Task(String description, Integer reward, Function<Player, Boolean> condition) {
+    public Task(Integer id, String description, Integer reward, Function<Player, Boolean> condition) {
+        this.id = id;
         this.description = description;
         this.reward = reward;
         this.condition = condition;
