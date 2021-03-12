b341dbe93dfa8612ac2ba61bc8a28bf18d25feb9
Valeria <vxp999@student.bham.ac.uk>
2021-03-12 17:59:55 +0200

    fix: change task entity to task UI, add listview

--- src/main/java/teamproject/wipeout/game/task/ui/TaskUI.java
+++ src/main/java/teamproject/wipeout/game/task/ui/TaskUI.java
@@ -1,38 +1,95 @@
 package teamproject.wipeout.game.task.ui;
 
+import javafx.collections.ObservableList;
+import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import teamproject.wipeout.engine.component.Transform;
+import teamproject.wipeout.engine.component.render.RectRenderable;
 import teamproject.wipeout.engine.component.render.RenderComponent;
 import teamproject.wipeout.engine.component.render.TextRenderable;
 import teamproject.wipeout.engine.component.ui.DialogUIComponent;
+import teamproject.wipeout.engine.core.GameScene;
+import teamproject.wipeout.engine.entity.GameEntity;
+import teamproject.wipeout.game.player.Player;
+import teamproject.wipeout.game.task.Task;
+
+import java.util.ArrayList;
 
 public class TaskUI extends VBox implements DialogUIComponent {
-    public String description;
+
+    public Transform transform;
+    public Point2D size;
+    public GameScene gameScene;
+
     private Pane parent;
 
-    public TaskUI(String description) {
+    private Integer MAX_TASKS = 5;
+
+    private StackPane stack;
+
+    private Rectangle rectangle = new Rectangle();
+    private Text[] texts = new Text[MAX_TASKS];
+    private ListView<Text> list = new ListView<>();
+
+
+    public TaskUI(Player player) {
         super();
 
-        this.setAlignment(Pos.CENTER);
+        this.rectangle.setX(0);
+        this.rectangle.setY(0);
+        this.rectangle.setWidth(150);
+        this.rectangle.setHeight(200);
+        this.rectangle.setFill(Color.BLACK);
+        this.rectangle.setMouseTransparent(true);
 
-//        TabPane tabPane = new TabPane();
-//        Tab buy = new Tab("Buy", new Label("Buy some items."));
-//        Tab sell = new Tab("Sell", new Label("Sell some items."));
-//        tabPane.getTabs().addAll(buy, sell);
-//        Button close = new Button("X");
-//        close.setOnAction(actionEvent -> this.parent.getChildren().remove(this));
-//
-//        this.getChildren().addAll(tabPane, close);
+        this.stack = new StackPane();
+        list.setMaxWidth(200);
+        list.setMaxHeight(200);
+        list.setMouseTransparent( true );
+        list.setFocusTraversable( false );
+        list.setStyle("-fx-stroke: black; -fx-stroke-width: 3;");
+
+        createTextRenderables();
+
+        showTasks(player.tasks);
+
+        this.stack.getChildren().addAll(rectangle, list);
+        this.getChildren().add(this.stack);
     }
 
+    public void showTasks(ArrayList<Task> tasks) {
+        int i = 0;
+        for(Task task: tasks) {
+            if (task.completed) {
+                continue;
+            }
+            list.getItems().get(i).setText(task.description);
+            i += 1;
+        }
+        while(i < MAX_TASKS) {
+            list.getItems().get(i).setText("");
+            i += 1;
+        }
+    }
+
+    private void createTextRenderables() {
+        for(int i = 0; i < MAX_TASKS; i++) {
+            texts[i] = new Text("");
+            texts[i].setFill(Color.BLACK);
+            texts[i].setMouseTransparent(true);
+            list.getItems().add(texts[i]);
+        }
+    }
     public void setParent(Pane parent) {
         this.parent = parent;
     }
