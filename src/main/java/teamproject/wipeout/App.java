5f81df9bd4b83b5cc147a0eb76d2e644187549e3
Jamie White <jmw981@student.bham.ac.uk>
2021-03-12 16:37:33 +0000

    Temporarially disabled AI Rat as it causes issues with networking

--- src/main/java/teamproject/wipeout/App.java
+++ src/main/java/teamproject/wipeout/App.java
@@ -160,7 +160,7 @@ public class App implements Controller {
         networker.worldEntity = world;
 
         //Currently broken in networking mode.
-        new AnimalEntity(gameScene, new Point2D(50, 50), world.getNavMesh(), spriteManager, new ArrayList<>(world.farms.values()));
+        //new AnimalEntity(gameScene, new Point2D(50, 50), world.getNavMesh(), spriteManager, new ArrayList<>(world.farms.values()));
 
         // Create tasks
         ArrayList<Task> allTasks = createAllTasks(itemStore);
