c239d1e67feb859b9709db92d05df0d424010e21
Oliver Little <oliver.little12@gmail.com>
2021-03-13 14:48:26 +0000

    Feature: hide-show button for Task UI

--- src/main/java/teamproject/wipeout/game/task/ui/TaskUI.java
+++ src/main/java/teamproject/wipeout/game/task/ui/TaskUI.java
@@ -1,40 +1,91 @@
 package teamproject.wipeout.game.task.ui;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 import teamproject.wipeout.game.player.Player;
 import teamproject.wipeout.game.task.Task;
+import teamproject.wipeout.util.resources.ResourceType;
 
 import java.util.ArrayList;
 
-public class TaskUI extends ListView<String> {
+public class TaskUI extends VBox {
+
+    private ListView<String> list;
+    private Button openCloseButton;
 
     private Integer MAX_TASKS = 5;
+
+    private boolean opened = true;
 
     public TaskUI(Player player) {
         super();
 
-        this.setMaxWidth(150);
-        this.setMaxHeight(200);
-        this.setMouseTransparent( true );
-        this.setFocusTraversable( false );
-        this.setStyle("-fx-stroke: black; -fx-stroke-width: 3;");
+        this.setSpacing(2);
+
+        this.getStylesheets().add(ResourceType.STYLESHEET.path + "task-ui.css");
+
+        openCloseButton = new Button("Hide Tasks");
+        openCloseButton.setOnAction(e -> {
+            opened = !opened;
+            this.setListVisible(opened);
+        });
+
+        list = new ListView<>();
+
+        list.setMaxWidth(120);
+        list.setMaxHeight(200);
+        list.setMouseTransparent( true );
+        list.setFocusTraversable( false );
+        list.setStyle("-fx-stroke: black; -fx-stroke-width: 3;");
+
+        this.getChildren().addAll(openCloseButton, list);
 
         showTasks(player.tasks);
+
+        
     }
 
     public void showTasks(ArrayList<Task> tasks) {
         int i = 0;
-        this.getItems().clear();
+        list.getItems().clear();
         for(Task task: tasks) {
             if (task.completed) {
                 continue;
             }
-            this.getItems().add(task.description);
+            list.getItems().add(task.description);
             i += 1;
         }
         while(i < MAX_TASKS) {
-            this.getItems().add("");
+            list.getItems().add("");
             i += 1;
         }
+    }
+
+    private void setListVisible(boolean visible) {
+        KeyValue goalWidth = null;
+        if (visible) {
+            openCloseButton.setText("Hide Tasks");
+
+            Rectangle clipRect = new Rectangle(0, 0, list.getWidth(), 0);
+            list.setClip(clipRect);
+            goalWidth = new KeyValue(clipRect.heightProperty(), list.getHeight(), Interpolator.EASE_OUT);
+        }
+        else {
+            openCloseButton.setText("Show Tasks");
+
+            Rectangle clipRect = new Rectangle(0, 0, list.getWidth(), list.getHeight());
+            list.setClip(clipRect);
+            goalWidth = new KeyValue(clipRect.heightProperty(), 0, Interpolator.EASE_IN);
+        }
+
+        KeyFrame frame = new KeyFrame(Duration.seconds(0.25), goalWidth);
+        Timeline timeline = new Timeline(frame);
+        timeline.play();
     }
 }
