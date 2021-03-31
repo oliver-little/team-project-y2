272309034f3fb1db3c5bfcfb6ad5a7a7e286fdf9
oliver-little <oliver.little12@gmail.com>
2021-03-31 17:27:22 +0100

    Fix player and tree offsets

--- src/main/java/teamproject/wipeout/App.java
+++ src/main/java/teamproject/wipeout/App.java
@@ -170,7 +170,7 @@ public class App implements Controller {
 
 
         try {
-            player.addComponent(new RenderComponent());
+            player.addComponent(new RenderComponent(new Point2D(0, -3)));
             player.addComponent(new PlayerAnimatorComponent(
                 spriteManager.getSpriteSet("player-red", "walk-up"), 
                 spriteManager.getSpriteSet("player-red", "walk-right"), 
