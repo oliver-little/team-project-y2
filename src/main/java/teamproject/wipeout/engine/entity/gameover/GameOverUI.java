e037cb12272848fd22eaed7f3df2bbc52a826892
Valeria <vxp999@student.bham.ac.uk>
2021-04-11 20:53:42 +0300

    feat: add players list

--- src/main/java/teamproject/wipeout/engine/entity/gameover/GameOverUI.java
+++ src/main/java/teamproject/wipeout/engine/entity/gameover/GameOverUI.java
@@ -1,11 +1,14 @@
 package teamproject.wipeout.engine.entity.gameover;
 
 import javafx.scene.control.Label;
+import teamproject.wipeout.game.player.Player;
 import teamproject.wipeout.util.resources.ResourceType;
+
+import java.util.HashMap;
 
 public class GameOverUI extends Label {
 
-    public GameOverUI() {
+    public GameOverUI(HashMap<Integer, Player> players) {
         super();
 
         this.setText("Game Over");
