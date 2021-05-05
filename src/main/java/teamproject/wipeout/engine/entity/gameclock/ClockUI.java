cf16cc2f11609c4759c8f0860cb7635a27998757
Valeria <vxp999@student.bham.ac.uk>
2021-05-05 13:52:21 +0300

    feat: add clock UI documetnation

--- src/main/java/teamproject/wipeout/engine/entity/gameclock/ClockUI.java
+++ src/main/java/teamproject/wipeout/engine/entity/gameclock/ClockUI.java
@@ -4,6 +4,9 @@ import javafx.scene.control.Label;
 import teamproject.wipeout.engine.entity.gameover.GameOverUI;
 import teamproject.wipeout.util.resources.ResourceType;
 
+/**
+ * Shows the Remaining time in a label in the top right of the screen
+ */
 public class ClockUI extends Label {
 
     public Runnable onEnd;
@@ -12,6 +15,11 @@ public class ClockUI extends Label {
     private Double time;
     private final Double initialTime;
 
+    /**
+     * Initialises the Clock UI
+     * @param time - the time to display in the label
+     * @param onEnd - closure to run when the time is up
+     */
     public ClockUI(double time, Runnable onEnd) {
         super();
         this.onEnd = onEnd;
@@ -28,14 +36,26 @@ public class ClockUI extends Label {
         this.getStylesheets().add(ResourceType.STYLESHEET.path + "game-ui.css");
     }
 
+    /**
+     * Get the time displayed on the label
+     * @return - displayed time
+     */
     public Double getTime() {
         return this.time;
     }
 
+    /**
+     * Get the information regarding whether the game is still running
+     * @return - a boolean for whether the game has ended or not
+     */
     public boolean getGameEnded() {
         return this.gameEnded;
     }
 
+    /**
+     * Showing the time, updating the UI
+     * @param timestep - the timeStep that has passed, with which we update the UI
+     */
     public void showTime(Double timestep) {
         if (gameEnded) {
             return;
