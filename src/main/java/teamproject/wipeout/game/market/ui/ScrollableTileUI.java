5487ca5a8302efd949c976435fb3f9d7c0ede2bd
Valeria <vxp999@student.bham.ac.uk>
2021-05-05 15:38:59 +0300

    feat: add search bar

--- src/main/java/teamproject/wipeout/game/market/ui/ScrollableTileUI.java
+++ src/main/java/teamproject/wipeout/game/market/ui/ScrollableTileUI.java
@@ -1,12 +1,19 @@
 package teamproject.wipeout.game.market.ui;
 
+//import java.awt.event.KeyEvent;
+import javafx.scene.input.KeyEvent;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.TilePane;
+import javafx.util.Pair;
 
 /**
  * Creates a pane with a ScrollPane inside a TilePane, with some given children.
@@ -14,9 +21,28 @@ import javafx.scene.layout.TilePane;
 public class ScrollableTileUI extends ScrollPane {
 
     private TilePane tilePane;
+    private Collection<Pair<Node, String>> allChildren;
 
-    public ScrollableTileUI(Collection<Node> children) {
+    public ScrollableTileUI(Collection<Pair<Node, String>> children, Boolean toShowSearchBar) {
         super();
+
+        allChildren = children;
+        TextField searchBar = new TextField();
+        if(toShowSearchBar) {
+            searchBar.addEventFilter(KeyEvent.ANY, e -> {
+                String text = searchBar.getText();
+
+                for(Pair<Node, String> node: allChildren) {
+                    if(node.getValue().contains(text)) {
+                        if(!tilePane.getChildren().contains(node.getKey())) {
+                            tilePane.getChildren().add(node.getKey());
+                        }
+                    } else {
+                        tilePane.getChildren().remove(node.getKey());
+                    }
+                }
+            });
+        }
 
         tilePane = new TilePane();
 
@@ -27,7 +53,13 @@ public class ScrollableTileUI extends ScrollPane {
         tilePane.setVgap(20);
         tilePane.setHgap(20);
 
-        tilePane.getChildren().addAll(children);
+        if(toShowSearchBar) {
+            tilePane.getChildren().add(searchBar);
+        }
+
+        for(Pair<Node, String> node: allChildren) {
+            tilePane.getChildren().add(node.getKey());
+        }
 
         this.setContent(tilePane);
     }
