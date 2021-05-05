90c861e92c9c0c26100cb053a8a2c9fe80827673
Valeria <vxp999@student.bham.ac.uk>
2021-05-05 13:52:38 +0300

    feat: add clock system documentation

--- src/main/java/teamproject/wipeout/engine/entity/gameclock/ClockSystem.java
+++ src/main/java/teamproject/wipeout/engine/entity/gameclock/ClockSystem.java
@@ -2,11 +2,20 @@ package teamproject.wipeout.engine.entity.gameclock;
 
 import teamproject.wipeout.engine.system.GameSystem;
 
+/**
+ * The Clock System that is responsible for updating the ClockUI
+ */
 public class ClockSystem implements GameSystem {
 
     public final ClockUI clockUI;
     public final Long gameStartTime;
 
+    /**
+     * Initialise the clock system
+     * @param duration - the duration of the game
+     * @param startTime - the initial time
+     * @param onEnd - closure to run when the time is up
+     */
     public ClockSystem(double duration, long startTime, Runnable onEnd) {
         this.clockUI = new ClockUI(duration, onEnd);
         this.gameStartTime = startTime;
