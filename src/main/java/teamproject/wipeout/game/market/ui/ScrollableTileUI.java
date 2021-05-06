181138c047236a112e15f309d029bf1ee095ea48
Oliver Little <oliver.little12@gmail.com>
2021-05-06 12:16:11 +0100

    Fix: items are hidden after using search

--- src/main/java/teamproject/wipeout/game/market/ui/ScrollableTileUI.java
+++ src/main/java/teamproject/wipeout/game/market/ui/ScrollableTileUI.java
@@ -10,6 +10,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
@@ -75,6 +76,7 @@ public class ScrollableTileUI extends VBox {
         }
 
         scrollPane.setContent(tilePane);
+        VBox.setVgrow(scrollPane, Priority.ALWAYS);
 
         this.getChildren().add(scrollPane);
     }
